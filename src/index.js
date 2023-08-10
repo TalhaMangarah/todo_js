20babc711b7cb16999d282d2c0cc7458ae4d0d69
TalhaMangarah <talha@talhamangarah.com>
2023-08-11 00:43:44 +0100

    - Updated addTask functionality. - Added remove and update task. - Added toggle completed function that adds line-through on task.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,63 @@
 const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
+const button = document.querySelector("button");
 
 taskInput.addEventListener('keydown', (event) => {
     if (event.key == 'Enter' && taskInput.value.trim() !== '') {
-        // taskList.append(taskInput.value);
         addTask(taskInput.value);
         taskInput.value = '';
     }
 });
 
 function addTask(taskText){
-    let taskItem = document.createElement('li');
+    let taskContainer = document.createElement('div');
+    taskContainer.classList.add('task-container');
+
+    let taskItem = document.createElement('span');
     taskItem.textContent = taskText;
-    taskList.appendChild(taskItem);
-};
+
+    let taskStatus = document.createElement('input');
+    taskStatus.type = 'checkbox';
+
+    let taskRemoveButton = document.createElement('button');
+    taskRemoveButton.type = 'button';
+    taskRemoveButton.id = 'taskRemoveButton';
+    taskRemoveButton.textContent = 'X';
+
+    taskContainer.appendChild(taskStatus);
+    taskContainer.appendChild(taskItem);
+    taskContainer.appendChild(taskRemoveButton);
+    taskList.appendChild(taskContainer);
+
+
+    taskRemoveButton.addEventListener('click', removeTask);
+    taskItem.addEventListener('dblclick', updateTask);
+    taskStatus.addEventListener('change', toggleCompleted);
+}
+
+function removeTask(event){
+    event.target.closest('.task-container').remove();
+}
+
+function updateTask(event){
+    const taskItem = event.target;
+    const taskText = taskItem.textContent;
+
+    const taskUpdateInput = document.createElement('input');
+    taskUpdateInput.value = taskText;
+
+    taskUpdateInput.addEventListener('keydown', (event) => {
+        if (event.key == 'Enter' && taskUpdateInput.value.trim !== ''){
+            taskItem.textContent = taskUpdateInput.value;
+            taskUpdateInput.replaceWith(taskItem);
+        }
+    });
+
+    taskItem.replaceWith(taskUpdateInput);
+    taskUpdateInput.focus();
+}
+
+function toggleCompleted(event){
+    const taskContainer = event.target.closest('.task-container');
+    taskContainer.classList.toggle('completed', event.target.checked);
+}
