4260b951dd793e7b4dc79bdb66fce40a7a6abb86
TalhaMangarah <talha@talhamangarah.com>
2023-08-15 01:17:10 +0100

    - Update taskArray to taskMap - Added IDs to each task to make it easier to manipulate

--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
 const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
-const button = document.querySelector("button");
+const importTasks = document.getElementById('importTasks');
+const exportTasks = document.getElementById('exportTasks');
+let taskMap = loadTasksFromLocalStorage();
+
+taskMap.forEach((taskText, taskId) => {
+    addTask(taskId, taskText)
+});
 
 taskInput.addEventListener('keydown', (event) => {
     if (event.key == 'Enter' && taskInput.value.trim() !== '') {
-        addTask(taskInput.value);
+        const taskId = generateUniqueId();
+        const taskText = taskInput.value;
+        addTask(taskId, taskText);
         taskInput.value = '';
+
+        taskMap.set(taskId, taskText);
+        saveTasksToLocalStorage(taskMap);
     }
 });
 
-function addTask(taskText){
+function loadTasksFromLocalStorage(){
+    const tasksJSON = localStorage.getItem('tasks');
+    return tasksJSON ? new Map(JSON.parse(tasksJSON)) : new Map();
+}
+
+function saveTasksToLocalStorage(tasks){
+    localStorage.setItem('tasks', JSON.stringify([...tasks]));
+}
+
+function addTask(taskId, taskText){
     let taskContainer = document.createElement('div');
     taskContainer.classList.add('task-container');
+    taskContainer.setAttribute('data-task-id', taskId);
 
     let taskItem = document.createElement('span');
+    taskItem.classList.add('task-text');
     taskItem.textContent = taskText;
 
     let taskStatus = document.createElement('input');
@@ -36,7 +58,13 @@ function addTask(taskText){
 }
 
 function removeTask(event){
-    event.target.closest('.task-container').remove();
+    const taskContainer = event.target.closest('.task-container');
+    const taskId = taskContainer.dataset.taskId;
+
+    taskContainer.remove();
+    taskMap.delete(taskId);
+
+    saveTasksToLocalStorage(taskMap);
 }
 
 function updateTask(event){
@@ -57,7 +85,20 @@ function updateTask(event){
     taskUpdateInput.focus();
 }
 
+function generateUniqueId(){
+    // https://stackoverflow.com/a/53116778
+    return Date.now().toString(36) + Math.random().toString(36).substring(2, 12).padStart(12, 0)
+}
+
 function toggleCompleted(event){
     const taskContainer = event.target.closest('.task-container');
     taskContainer.classList.toggle('completed', event.target.checked);
 }
+
+// TODO - implement import tasks from file functionality
+importTasks.addEventListener('click', (event) => {
+});
+
+// TODO - implement export tasks from file functionality
+exportTasks.addEventListener('click', (event) => {
+});
