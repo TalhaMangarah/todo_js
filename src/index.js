f0846f302fa8e8b61c6d07bbf7ad9848992b02ea
TalhaMangarah <talha@talhamangarah.com>
2023-09-07 18:06:17 +0100

    - Changed colours - Added animations - Import tasks still needs working on

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
+const fileInput = document.getElementById('fileInput');
 const importTasks = document.getElementById('importTasks');
 const exportTasks = document.getElementById('exportTasks');
+const removeIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="task-remove-icon" viewBox="0 0 16 16"><path d="M14,3 C14.5522847,3 15,3.44771525 15,4 C15,4.55228475 14.5522847,5 14,5 L13.846,5 L13.1420511,14.1534404 C13.0618518,15.1954311 12.1930072,16 11.1479,16 L4.85206,16 C3.80698826,16 2.93809469,15.1953857 2.8579545,14.1533833 L2.154,5 L2,5 C1.44771525,5 1,4.55228475 1,4 C1,3.44771525 1.44771525,3 2,3 L5,3 L5,2 C5,0.945642739 5.81588212,0.0818352903 6.85073825,0.00548576453 L7,0 L9,0 C10.0543573,0 10.9181647,0.815882118 10.9945142,1.85073825 L11,2 L11,3 L14,3 Z M11.84,5 L4.159,5 L4.85206449,14.0000111 L11.1479,14.0000111 L11.84,5 Z M9,2 L7,2 L7,3 L9,3 L9,2 Z"/></svg>';
+const checkmarkIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="task-checkmark-icon" viewBox="0 0 16 16"><path d="M13.7071,4.29289 C14.0976,4.68342 14.0976,5.31658 13.7071,5.70711 L7.70711,11.7071 C7.31658,12.0976 6.68342,12.0976 6.29289,11.7071 L3.29289,8.70711 C2.90237,8.31658 2.90237,7.68342 3.29289,7.29289 C3.68342,6.90237 4.31658,6.90237 4.70711,7.29289 L7,9.58579 L12.2929,4.29289 C12.6834,3.90237 13.3166,3.90237 13.7071,4.29289 Z"/></svg>';
 let taskMap = loadTasksFromLocalStorage();
 
 taskMap.forEach((task, taskId) => {
@@ -47,24 +50,38 @@ function addTask(taskId, task){
     let taskStatusChecked = document.createElement('input');
     taskStatusChecked.type = 'checkbox';
     taskStatusChecked.classList.add('task-status');
+    taskStatusChecked.id = 'taskStatus';
     taskStatusChecked.checked = task.completed;
+
+    let taskStatusCheckmark = document.createElement('span');
+    taskStatusCheckmark.classList.add('checkmark');
+
+    let taskStatusLabel = document.createElement('label');
+    // taskStatusLabel.htmlFor = 'taskStatus';
+    taskStatusLabel.classList.add('checkbox-label');
+
+    taskStatusLabel.appendChild(taskStatusChecked);
+    taskStatusLabel.appendChild(taskStatusCheckmark);
+
     if (task.completed){
         taskContainer.classList.toggle('completed');
+        taskStatusCheckmark.innerHTML = checkmarkIcon;
     }
 
     let taskRemoveButton = document.createElement('button');
     taskRemoveButton.type = 'button';
     taskRemoveButton.id = 'taskRemoveButton';
-    taskRemoveButton.textContent = 'X';
+    taskRemoveButton.innerHTML = removeIcon;
 
-    taskContainer.appendChild(taskStatusChecked);
+    // taskContainer.appendChild(taskStatusChecked);
+    taskContainer.appendChild(taskStatusLabel);
     taskContainer.appendChild(taskItem);
     taskContainer.appendChild(taskRemoveButton);
     taskList.appendChild(taskContainer);
 
 
     taskRemoveButton.addEventListener('click', removeTask);
-    taskItem.addEventListener('dblclick', updateTask);
+    taskItem.addEventListener('click', updateTask);
     taskStatusChecked.addEventListener('change', toggleCompleted);
 }
 
@@ -89,7 +106,7 @@ function updateTask(event){
     taskUpdateInput.value = taskText;
 
     taskUpdateInput.addEventListener('keydown', (event) => {
-        if (event.key == 'Enter' && taskUpdateInput.value.trim !== ''){
+        if (event.key == 'Enter' && taskUpdateInput.value.trim() !== ''){
             taskItem.textContent = taskUpdateInput.value;
             task.text = taskItem.textContent;
             taskMap.set(taskId, task);
@@ -112,25 +129,108 @@ function toggleCompleted(event){
     const taskContainer = event.target.closest('.task-container');
     const taskId = taskContainer.dataset.taskId;
     const taskStatusCheckbox = taskContainer.querySelector('.task-status');
+    const taskStatusLabel = taskContainer.querySelector('.checkmark');
     taskContainer.classList.toggle('completed', taskStatusCheckbox.checked);
 
     const task = taskMap.get(taskId);
 
     task.completed = !task.completed;
+    if(task.completed){
+        taskStatusLabel.innerHTML = checkmarkIcon;
+    }
+    else {
+        taskStatusLabel.innerHTML = '';
+    }
+
     taskMap.set(taskId, task);
     saveTasksToLocalStorage(taskMap);
 }
 
 // TODO - implement import tasks from file functionality
-importTasks.addEventListener('click', (event) => {
+importTasks.addEventListener('click', () => {
+    fileInput.click();
 });
+
+
+// "Keys" here are due to the arr that will be converted to a Map.
+function areKeysUnique(arr) {
+    const seenKeys = new Set();
+    for (const subArray of arr) {
+        const key = subArray[0];
+        if (seenKeys.has(key)) {
+            return false; // Duplicate key found
+        }
+        seenKeys.add(key);
+    }
+    return true; // All keys are unique
+}
+
+
+fileInput.addEventListener('change', (event) => {
+    const selectedFile = event.target.files[0]; // Get the selected file
+    if (selectedFile.length == 0) return; 
+
+    let reader = new FileReader();
+
+    reader.onload = (e) => {
+        const file = JSON.parse(e.target.result);
+
+        try{
+            if(!areKeysUnique(file)){
+                throw new Error('Task IDs are not unique');
+            }
+            console.log(file);
+        }
+        catch(error){
+            console.error(error);
+            alert('Error with file that was imported.\nPlease confirm formatting.\nE.g. [["unique task ID string",{"text":"Task text 1","completed":true/false}],["unique task ID string",{"text":"Task text 2","completed":true/false}]]');
+            return;
+        }
+        // console.log(fileMap);
+        // console.log(JSON.parse(file));
+        // if(file.substr(0,2) != '[[' || file.substr(file.length-2, 2) != ']]'){
+        //     console.log('file not in correct format');
+        //     return;
+        // }
+    }
+
+    reader.onerror = (e) => alert(e.target.error.name);
+
+    reader.readAsText(selectedFile);
+
+    // if (selectedFile) {
+        // Call a function to handle the file import
+        // console.log(selectedFile);
+
+        // let reader = new FileReader();
+
+        // reader.onload = (e) => {
+        //     const file = e.target.result;
+        //     if(file[0] !== '[' || file[file.length-1] !== ']'){
+        //         console.log('file not in correct format');
+        //         return;
+        //     }
+        // }
+
+        // reader.onerror = (e) => alert(e.target.error.name);
+
+        // reader.readAsText(selectedFile);
+
+        // (async () => {
+        //     const fileContent = await selectedFile.text();
+        //     // console.log(fileContent[fileContent.length-1]);
+        // })();
+        // handleFileImport(selectedFile);
+    // }
+});
+
+// function handleFileImport(file){
+
+// }
 
 exportTasks.addEventListener('click', (event) => {
     const tasksJSON = localStorage.getItem('tasks');
-    console.log(tasksJSON);
     if (tasksJSON !== "[]"){
-        console.log('exporting');
-
         const timestamp = new Date().toISOString().replace(/[:.]/g, '');
         const fileName = `tasks_${timestamp}.txt`; 
         
